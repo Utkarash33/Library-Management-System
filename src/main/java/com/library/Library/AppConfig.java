3bf4be3438f423018441bef50c9143c53c3480bc
Utkarash Thakur <utkarash.thakur@hyperface.co>
2024-04-22 18:24:00 +0530

    Fix the appConfiguration for the redis to write the custom queries

---
+++ src/main/java/com/library/Library/AppConfig.java
@@ -0,0 +1,30 @@
+package com.library.Library;
+
+import com.library.Library.entities.Book;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Book> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Book> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        return template;
+    }
+
+}
--- src/main/java/com/library/Library/service/BookSerivceImplRedis.java
+++ src/main/java/com/library/Library/service/BookSerivceImplRedis.java
@@ -5,22 +5,52 @@ import com.library.Library.repository.BookRepository;
 import com.library.Library.repository.RedisRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.Set;
 
 @Service
 @ConditionalOnProperty(name ="db_flag", havingValue = "redis")
 public class BookSerivceImplRedis implements BookService{
     @Autowired
     private RedisRepo bookRepository;
+
+    @Autowired
+    private RedisTemplate<String,Book> redisTemplate;
+
+
+
     @Override
     public String addBook(Book book) {
 
-        List<Book> book2 = bookRepository.searchByTitleIgnoreCaseAndAuthorIgnoreCase(book.getTitle(),book.getAuthor());
-        if(book2.size()!=0)
+        List<Book> books = new ArrayList<>();
+
+        try(Jedis jedis = new Jedis("redis://localhost:6379")){
+            Set<String> bookKeys = jedis.smembers("books");
+            for (String key : bookKeys) {
+                Map<String, String> bookData = jedis.hgetAll("books:" + key); // Assuming each book key is "books:{id}"
+
+                if (bookData != null && !bookData.isEmpty() && bookData.containsKey("author") && bookData.containsKey("title") && bookData.containsKey("id")) {
+                    Book b = new Book();
+                    b.setId(Long.parseLong(bookData.get("id")));
+                    b.setAuthor(bookData.get("author"));
+                    b.setTitle(bookData.get("title"));
+                    b.setReserved(Boolean.parseBoolean(bookData.get("reserved")));
+                    if (b.getTitle().toLowerCase().contains(book.getTitle().toLowerCase())
+                            && b.getAuthor().toLowerCase().contains(book.getAuthor().toLowerCase())) {
+                        books.add(b);
+                    }
+                }
+            }
+
+        }
+        if(books.size()!=0)
         {
             throw new IllegalArgumentException("Book already exists");
         }
@@ -67,13 +97,32 @@ public class BookSerivceImplRedis implements BookService{
 
     @Override
     public List<Book> searchBooks(String query) {
-        List<Book> listOfBooks = bookRepository.findByTitle(query);
+        List<Book> books = new ArrayList<>();
 
-        System.out.println(listOfBooks);
-        if(listOfBooks.size()==0)
+        try(Jedis jedis = new Jedis("redis://localhost:6379")){
+            Set<String> bookKeys = jedis.smembers("books");
+            for (String key : bookKeys) {
+                Map<String, String> bookData = jedis.hgetAll("books:" + key);
+                System.out.println(bookData);
+
+                if (bookData != null && !bookData.isEmpty()) {
+                    Book b = new Book();
+                    b.setId(Long.parseLong(bookData.get("id")));
+                    b.setAuthor(bookData.get("author"));
+                    b.setTitle(bookData.get("title"));
+                    b.setReserved(Boolean.parseBoolean(bookData.get("reserved")));
+                    System.out.println(b.getTitle().toLowerCase()+" "+query);
+                    if (b.getTitle().toLowerCase().contains(query.toLowerCase())) {
+                        books.add(b);
+                    }
+                }
+            }
+
+        }
+        if(books.size()==0)
         {
             throw new IllegalArgumentException("No book found with "+query);
         }
-        return listOfBooks;
+        return books;
     }
 }
